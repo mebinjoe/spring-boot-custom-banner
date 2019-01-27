c5017ad748be9b7380c42af372a8628cde18ac92
mebinjoe <mebinjoe@gmail.com>
2019-01-27 16:22:21 +0530

    Added Rest Controller, Service, Sample Banner

---
+++ src/main/java/app/controller/Controller.java
@@ -0,0 +1,38 @@
+package app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+import app.service.UriService;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    UriService uriService;
+
+    @PostMapping(path="renderBanner")
+    private void renderBanner(@RequestBody Map<String, String> payload) {
+        if (payload != null){
+            String text = payload.get("text");
+            String font = payload.get("font");
+            String uri = "https://devops.datenkollektiv.de/renderBannerTxt";
+
+            try {
+                uri = String.valueOf(uriService.appendUri(uri+"?text="+text, "font="+font));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(uri, String.class);
+            System.out.println(result);
+        }
+    }
+
+
+}
---
+++ src/main/java/app/service/UriService.java
@@ -0,0 +1,22 @@
+package app.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class UriService {
+    public URI appendUri(String uri, String appendQuery) throws URISyntaxException {
+        URI oldUri = new URI(uri);
+
+        String newQuery = oldUri.getQuery();
+        if (newQuery == null) {
+            newQuery = appendQuery;
+        } else {
+            newQuery += "&" + appendQuery;
+        }
+
+        URI newUri = new URI(oldUri.getScheme(), oldUri.getAuthority(),
+                oldUri.getPath(), newQuery, oldUri.getFragment());
+
+        return newUri;
+    }
+}
